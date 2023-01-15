0134d217d1792cd2afb1b3cb03dc4fa8b2bd8e8b
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2023-01-15 11:24:27 +0530

    Video 24 full

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a News Components
-        <NewsItem/>
+      <div className="container my-3">
+       <h2>New Total - Top Headlines</h2>
+       <div className="row">
+            <div className="col-md-4">
+                <NewsItem title="myTitle" description="mydesc"/>
+            </div>
+            <div className="col-md-4">
+                <NewsItem title="myTitle" description="mydesc"/>
+            </div>
+            <div className="col-md-4">
+                <NewsItem title="myTitle" description="mydesc"/>
+            </div>
+      </div>
       </div>
     )
   }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ export class NavBar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">NewsMonkey</a>
+                        <a className="navbar-brand" href="/">News-Total</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                         </button>
