568a9b60a5ec4e9b19c59e9e6bcf4557607464ec
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2023-01-20 22:38:26 +0530

    video 31 done

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import {Link} from "react-router-dom"
 
-export class NavBar extends Component {
-
+export  class NavBar extends Component {
     render() {
         return (
             <div>
@@ -12,22 +12,30 @@ export class NavBar extends Component {
                         <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="/">Home</a>
-                            </li>
-                            <li className="nav-item"><a className="nav-link" href="/about">About</a> </li>
-                            <li className="nav-item"><a className="nav-link" href="/about">business</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/about">entertainment</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/about">general</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/about">health</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/about">science</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/about">sports</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/about">technology</a></li>
-                             
-                          
-                        </ul>
-                        </div>
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link active" aria-current="page" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/business"> business</Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/entertainment"> entertainment</Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/general"> general</Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/health"> health</Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/science"> science</Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/sports"> sports</Link></li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/technology"> technology</Link></li>
+              </ul>
+              </div>
                     </div>
                     </nav>
             </div>
