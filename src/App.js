9246420e1cd4a370fbf37b9cfafc9fb9e801a65c
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2023-01-28 19:53:04 +0530

    Now all done

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App=()=>{
  
     return (
       <div>
-        <Router >
+        <Router>
         <NavBar/>
 
         <LoadingBar
@@ -28,7 +28,7 @@ const App=()=>{
             progress={progress}
         />
         <Routes>
-          <Route exact path="/News-App" element={<News setProgress={setProgress} apiKey={apiKey}  key="general" pageSize={pageSize} country="in" category="general"/>}/>
+          <Route exact path="/" element={<News setProgress={setProgress} apiKey={apiKey}  key="general" pageSize={pageSize} country="in" category="general"/>}/>
           <Route exact path="/business" element={<News setProgress={setProgress} apiKey={apiKey}  key="business" pageSize={pageSize} country="in" category="business"/>}/> 
           <Route exact path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey}  key="entertainment" pageSize={pageSize} country="in" category="entertainment"/>}/> 
           <Route exact path="/general" element={<News setProgress={setProgress} apiKey={apiKey}  key="general" pageSize={pageSize} country="in" category="general"/>}/> 
